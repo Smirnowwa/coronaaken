1e801083f5407ad599633d26ec41253a249857ed
Alina Smirnova <alismi@ttu.ee>
2022-04-04 20:02:08 +0300

    Add project files.

---
+++ coronaaken/Form1.Designer.vb
@@ -0,0 +1,429 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class FrmMain
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.components = New System.ComponentModel.Container()
+        Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(FrmMain))
+        Me.Panel2 = New System.Windows.Forms.Panel()
+        Me.Panel1 = New System.Windows.Forms.Panel()
+        Me.btnMaailmas = New System.Windows.Forms.Button()
+        Me.BunifuElipse2 = New Bunifu.Framework.UI.BunifuElipse(Me.components)
+        Me.Button2 = New System.Windows.Forms.Button()
+        Me.BunifuElipse4 = New Bunifu.Framework.UI.BunifuElipse(Me.components)
+        Me.Button1 = New System.Windows.Forms.Button()
+        Me.BunifuElipse3 = New Bunifu.Framework.UI.BunifuElipse(Me.components)
+        Me.BunifuElipse5 = New Bunifu.Framework.UI.BunifuElipse(Me.components)
+        Me.BunifuElipse6 = New Bunifu.Framework.UI.BunifuElipse(Me.components)
+        Me.Panel3 = New System.Windows.Forms.Panel()
+        Me.Panel4 = New System.Windows.Forms.Panel()
+        Me.BunifuElipse1 = New Bunifu.Framework.UI.BunifuElipse(Me.components)
+        Me.BunifuElipse7 = New Bunifu.Framework.UI.BunifuElipse(Me.components)
+        Me.PictureBox2 = New System.Windows.Forms.PictureBox()
+        Me.PictureBox1 = New System.Windows.Forms.PictureBox()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.Label4 = New System.Windows.Forms.Label()
+        Me.Label5 = New System.Windows.Forms.Label()
+        Me.Label6 = New System.Windows.Forms.Label()
+        Me.Label7 = New System.Windows.Forms.Label()
+        Me.Label8 = New System.Windows.Forms.Label()
+        Me.Label9 = New System.Windows.Forms.Label()
+        Me.Label10 = New System.Windows.Forms.Label()
+        Me.Label11 = New System.Windows.Forms.Label()
+        Me.Label12 = New System.Windows.Forms.Label()
+        Me.ComboBox1 = New System.Windows.Forms.ComboBox()
+        Me.BunifuElipse8 = New Bunifu.Framework.UI.BunifuElipse(Me.components)
+        Me.Button3 = New System.Windows.Forms.Button()
+        Me.BunifuElipse9 = New Bunifu.Framework.UI.BunifuElipse(Me.components)
+        Me.TextBox1 = New System.Windows.Forms.TextBox()
+        Me.Panel2.SuspendLayout()
+        Me.Panel1.SuspendLayout()
+        CType(Me.PictureBox2, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.PictureBox1, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.SuspendLayout()
+        '
+        'Panel2
+        '
+        Me.Panel2.BackColor = System.Drawing.Color.FromArgb(CType(CType(221, Byte), Integer), CType(CType(255, Byte), Integer), CType(CType(208, Byte), Integer))
+        Me.Panel2.Controls.Add(Me.Label12)
+        Me.Panel2.Controls.Add(Me.Label11)
+        Me.Panel2.Controls.Add(Me.Label10)
+        Me.Panel2.Controls.Add(Me.Label9)
+        Me.Panel2.Controls.Add(Me.Label8)
+        Me.Panel2.Controls.Add(Me.Label7)
+        Me.Panel2.Controls.Add(Me.Label6)
+        Me.Panel2.Controls.Add(Me.Label5)
+        Me.Panel2.Controls.Add(Me.Label4)
+        Me.Panel2.Controls.Add(Me.Label3)
+        Me.Panel2.Location = New System.Drawing.Point(12, 160)
+        Me.Panel2.Margin = New System.Windows.Forms.Padding(4, 4, 4, 4)
+        Me.Panel2.Name = "Panel2"
+        Me.Panel2.Size = New System.Drawing.Size(292, 646)
+        Me.Panel2.TabIndex = 1
+        '
+        'Panel1
+        '
+        Me.Panel1.BackColor = System.Drawing.Color.FromArgb(CType(CType(136, Byte), Integer), CType(CType(194, Byte), Integer), CType(CType(120, Byte), Integer))
+        Me.Panel1.Controls.Add(Me.Label1)
+        Me.Panel1.Controls.Add(Me.PictureBox1)
+        Me.Panel1.Location = New System.Drawing.Point(12, 12)
+        Me.Panel1.Margin = New System.Windows.Forms.Padding(4, 4, 4, 4)
+        Me.Panel1.Name = "Panel1"
+        Me.Panel1.Size = New System.Drawing.Size(1629, 140)
+        Me.Panel1.TabIndex = 2
+        '
+        'btnMaailmas
+        '
+        Me.btnMaailmas.BackColor = System.Drawing.Color.FromArgb(CType(CType(136, Byte), Integer), CType(CType(194, Byte), Integer), CType(CType(120, Byte), Integer))
+        Me.btnMaailmas.FlatAppearance.BorderSize = 0
+        Me.btnMaailmas.FlatStyle = System.Windows.Forms.FlatStyle.Flat
+        Me.btnMaailmas.ForeColor = System.Drawing.Color.White
+        Me.btnMaailmas.Location = New System.Drawing.Point(425, 662)
+        Me.btnMaailmas.Margin = New System.Windows.Forms.Padding(4, 4, 4, 4)
+        Me.btnMaailmas.Name = "btnMaailmas"
+        Me.btnMaailmas.Size = New System.Drawing.Size(160, 48)
+        Me.btnMaailmas.TabIndex = 6
+        Me.btnMaailmas.Text = "Maailmas"
+        Me.btnMaailmas.UseVisualStyleBackColor = False
+        '
+        'BunifuElipse2
+        '
+        Me.BunifuElipse2.ElipseRadius = 45
+        Me.BunifuElipse2.TargetControl = Me.btnMaailmas
+        '
+        'Button2
+        '
+        Me.Button2.BackColor = System.Drawing.Color.FromArgb(CType(CType(136, Byte), Integer), CType(CType(194, Byte), Integer), CType(CType(120, Byte), Integer))
+        Me.Button2.FlatAppearance.BorderSize = 0
+        Me.Button2.FlatStyle = System.Windows.Forms.FlatStyle.Flat
+        Me.Button2.ForeColor = System.Drawing.Color.White
+        Me.Button2.Location = New System.Drawing.Point(1064, 662)
+        Me.Button2.Margin = New System.Windows.Forms.Padding(4, 4, 4, 4)
+        Me.Button2.Name = "Button2"
+        Me.Button2.Size = New System.Drawing.Size(160, 48)
+        Me.Button2.TabIndex = 8
+        Me.Button2.Text = "USAs"
+        Me.Button2.UseVisualStyleBackColor = False
+        '
+        'BunifuElipse4
+        '
+        Me.BunifuElipse4.ElipseRadius = 45
+        Me.BunifuElipse4.TargetControl = Me.Button2
+        '
+        'Button1
+        '
+        Me.Button1.BackColor = System.Drawing.Color.FromArgb(CType(CType(136, Byte), Integer), CType(CType(194, Byte), Integer), CType(CType(120, Byte), Integer))
+        Me.Button1.FlatAppearance.BorderSize = 0
+        Me.Button1.FlatStyle = System.Windows.Forms.FlatStyle.Flat
+        Me.Button1.ForeColor = System.Drawing.Color.White
+        Me.Button1.Location = New System.Drawing.Point(739, 662)
+        Me.Button1.Margin = New System.Windows.Forms.Padding(4, 4, 4, 4)
+        Me.Button1.Name = "Button1"
+        Me.Button1.Size = New System.Drawing.Size(160, 48)
+        Me.Button1.TabIndex = 9
+        Me.Button1.Text = "Euroopas"
+        Me.Button1.UseVisualStyleBackColor = False
+        '
+        'BunifuElipse3
+        '
+        Me.BunifuElipse3.ElipseRadius = 45
+        Me.BunifuElipse3.TargetControl = Me.Button1
+        '
+        'BunifuElipse5
+        '
+        Me.BunifuElipse5.ElipseRadius = 45
+        Me.BunifuElipse5.TargetControl = Me.Panel1
+        '
+        'BunifuElipse6
+        '
+        Me.BunifuElipse6.ElipseRadius = 45
+        Me.BunifuElipse6.TargetControl = Me.Panel2
+        '
+        'Panel3
+        '
+        Me.Panel3.BackColor = System.Drawing.Color.FromArgb(CType(CType(221, Byte), Integer), CType(CType(255, Byte), Integer), CType(CType(208, Byte), Integer))
+        Me.Panel3.Location = New System.Drawing.Point(1345, 231)
+        Me.Panel3.Margin = New System.Windows.Forms.Padding(4)
+        Me.Panel3.Name = "Panel3"
+        Me.Panel3.Size = New System.Drawing.Size(296, 199)
+        Me.Panel3.TabIndex = 10
+        '
+        'Panel4
+        '
+        Me.Panel4.BackColor = System.Drawing.Color.FromArgb(CType(CType(221, Byte), Integer), CType(CType(255, Byte), Integer), CType(CType(208, Byte), Integer))
+        Me.Panel4.Location = New System.Drawing.Point(1345, 558)
+        Me.Panel4.Margin = New System.Windows.Forms.Padding(4)
+        Me.Panel4.Name = "Panel4"
+        Me.Panel4.Size = New System.Drawing.Size(296, 199)
+        Me.Panel4.TabIndex = 11
+        '
+        'BunifuElipse1
+        '
+        Me.BunifuElipse1.ElipseRadius = 45
+        Me.BunifuElipse1.TargetControl = Me.Panel3
+        '
+        'BunifuElipse7
+        '
+        Me.BunifuElipse7.ElipseRadius = 45
+        Me.BunifuElipse7.TargetControl = Me.Panel4
+        '
+        'PictureBox2
+        '
+        Me.PictureBox2.BackgroundImage = CType(resources.GetObject("PictureBox2.BackgroundImage"), System.Drawing.Image)
+        Me.PictureBox2.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom
+        Me.PictureBox2.Location = New System.Drawing.Point(496, 184)
+        Me.PictureBox2.Name = "PictureBox2"
+        Me.PictureBox2.Size = New System.Drawing.Size(72, 68)
+        Me.PictureBox2.TabIndex = 12
+        Me.PictureBox2.TabStop = False
+        '
+        'PictureBox1
+        '
+        Me.PictureBox1.BackgroundImage = Global.coronaaken.My.Resources.Resources.mask
+        Me.PictureBox1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom
+        Me.PictureBox1.ErrorImage = Global.coronaaken.My.Resources.Resources.mask
+        Me.PictureBox1.Location = New System.Drawing.Point(345, 14)
+        Me.PictureBox1.Name = "PictureBox1"
+        Me.PictureBox1.Size = New System.Drawing.Size(121, 101)
+        Me.PictureBox1.TabIndex = 12
+        Me.PictureBox1.TabStop = False
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Font = New System.Drawing.Font("Microsoft Sans Serif", 25.875!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label1.Location = New System.Drawing.Point(570, 26)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(487, 79)
+        Me.Label1.TabIndex = 13
+        Me.Label1.Text = "Püsige terved!"
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Location = New System.Drawing.Point(609, 205)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(441, 25)
+        Me.Label2.TabIndex = 13
+        Me.Label2.Text = "nakkatumise protsent = % elanikkonna kohta"
+        '
+        'Label3
+        '
+        Me.Label3.AutoSize = True
+        Me.Label3.Location = New System.Drawing.Point(37, 45)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(196, 25)
+        Me.Label3.TabIndex = 14
+        Me.Label3.Text = "COVID nõuandetel."
+        '
+        'Label4
+        '
+        Me.Label4.AutoSize = True
+        Me.Label4.Font = New System.Drawing.Font("Microsoft Sans Serif", 10.875!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label4.Location = New System.Drawing.Point(36, 86)
+        Me.Label4.Name = "Label4"
+        Me.Label4.Size = New System.Drawing.Size(83, 33)
+        Me.Label4.TabIndex = 14
+        Me.Label4.Text = "1220"
+        '
+        'Label5
+        '
+        Me.Label5.AutoSize = True
+        Me.Label5.Location = New System.Drawing.Point(37, 151)
+        Me.Label5.Name = "Label5"
+        Me.Label5.Size = New System.Drawing.Size(223, 25)
+        Me.Label5.TabIndex = 14
+        Me.Label5.Text = "Peamised sümptomid:"
+        '
+        'Label6
+        '
+        Me.Label6.AutoSize = True
+        Me.Label6.Location = New System.Drawing.Point(42, 187)
+        Me.Label6.Name = "Label6"
+        Me.Label6.Size = New System.Drawing.Size(101, 25)
+        Me.Label6.TabIndex = 14
+        Me.Label6.Text = "- peavalu"
+        '
+        'Label7
+        '
+        Me.Label7.AutoSize = True
+        Me.Label7.Location = New System.Drawing.Point(42, 216)
+        Me.Label7.Name = "Label7"
+        Me.Label7.Size = New System.Drawing.Size(221, 25)
+        Me.Label7.TabIndex = 15
+        Me.Label7.Text = "- haistmismeelte kadu"
+        '
+        'Label8
+        '
+        Me.Label8.AutoSize = True
+        Me.Label8.Location = New System.Drawing.Point(42, 245)
+        Me.Label8.Name = "Label8"
+        Me.Label8.Size = New System.Drawing.Size(145, 25)
+        Me.Label8.TabIndex = 16
+        Me.Label8.Text = "- ninakinnisus"
+        '
+        'Label9
+        '
+        Me.Label9.AutoSize = True
+        Me.Label9.Location = New System.Drawing.Point(42, 340)
+        Me.Label9.Name = "Label9"
+        Me.Label9.Size = New System.Drawing.Size(206, 25)
+        Me.Label9.TabIndex = 17
+        Me.Label9.Text = "Viirusest hoidumine:"
+        '
+        'Label10
+        '
+        Me.Label10.AutoSize = True
+        Me.Label10.Location = New System.Drawing.Point(42, 385)
+        Me.Label10.Name = "Label10"
+        Me.Label10.Size = New System.Drawing.Size(117, 25)
+        Me.Label10.TabIndex = 18
+        Me.Label10.Text = "- pese käsi"
+        '
+        'Label11
+        '
+        Me.Label11.AutoSize = True
+        Me.Label11.Location = New System.Drawing.Point(42, 410)
+        Me.Label11.Name = "Label11"
+        Me.Label11.Size = New System.Drawing.Size(158, 25)
+        Me.Label11.TabIndex = 19
+        Me.Label11.Text = "- liigu hajutatult"
+        '
+        'Label12
+        '
+        Me.Label12.AutoSize = True
+        Me.Label12.Location = New System.Drawing.Point(42, 435)
+        Me.Label12.Name = "Label12"
+        Me.Label12.Size = New System.Drawing.Size(146, 25)
+        Me.Label12.TabIndex = 20
+        Me.Label12.Text = "- kanna maski"
+        '
+        'ComboBox1
+        '
+        Me.ComboBox1.BackColor = System.Drawing.Color.FromArgb(CType(CType(136, Byte), Integer), CType(CType(194, Byte), Integer), CType(CType(120, Byte), Integer))
+        Me.ComboBox1.FormattingEnabled = True
+        Me.ComboBox1.Location = New System.Drawing.Point(705, 270)
+        Me.ComboBox1.Name = "ComboBox1"
+        Me.ComboBox1.Size = New System.Drawing.Size(260, 33)
+        Me.ComboBox1.TabIndex = 14
+        '
+        'BunifuElipse8
+        '
+        Me.BunifuElipse8.ElipseRadius = 45
+        Me.BunifuElipse8.TargetControl = Me.ComboBox1
+        '
+        'Button3
+        '
+        Me.Button3.BackColor = System.Drawing.Color.FromArgb(CType(CType(136, Byte), Integer), CType(CType(194, Byte), Integer), CType(CType(120, Byte), Integer))
+        Me.Button3.FlatAppearance.BorderSize = 0
+        Me.Button3.FlatStyle = System.Windows.Forms.FlatStyle.Flat
+        Me.Button3.Font = New System.Drawing.Font("Microsoft Sans Serif", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Button3.ForeColor = System.Drawing.Color.White
+        Me.Button3.Location = New System.Drawing.Point(705, 392)
+        Me.Button3.Margin = New System.Windows.Forms.Padding(4)
+        Me.Button3.Name = "Button3"
+        Me.Button3.Size = New System.Drawing.Size(248, 72)
+        Me.Button3.TabIndex = 15
+        Me.Button3.Text = "Otsing"
+        Me.Button3.UseVisualStyleBackColor = False
+        '
+        'BunifuElipse9
+        '
+        Me.BunifuElipse9.ElipseRadius = 65
+        Me.BunifuElipse9.TargetControl = Me.Button3
+        '
+        'TextBox1
+        '
+        Me.TextBox1.Location = New System.Drawing.Point(775, 331)
+        Me.TextBox1.Name = "TextBox1"
+        Me.TextBox1.Size = New System.Drawing.Size(100, 31)
+        Me.TextBox1.TabIndex = 16
+        '
+        'FrmMain
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(12.0!, 25.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.BackColor = System.Drawing.Color.White
+        Me.ClientSize = New System.Drawing.Size(1654, 819)
+        Me.Controls.Add(Me.TextBox1)
+        Me.Controls.Add(Me.Button3)
+        Me.Controls.Add(Me.ComboBox1)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.PictureBox2)
+        Me.Controls.Add(Me.Panel4)
+        Me.Controls.Add(Me.Panel3)
+        Me.Controls.Add(Me.Button1)
+        Me.Controls.Add(Me.Button2)
+        Me.Controls.Add(Me.btnMaailmas)
+        Me.Controls.Add(Me.Panel1)
+        Me.Controls.Add(Me.Panel2)
+        Me.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None
+        Me.Margin = New System.Windows.Forms.Padding(4, 4, 4, 4)
+        Me.Name = "FrmMain"
+        Me.Text = "Form1"
+        Me.Panel2.ResumeLayout(False)
+        Me.Panel2.PerformLayout()
+        Me.Panel1.ResumeLayout(False)
+        Me.Panel1.PerformLayout()
+        CType(Me.PictureBox2, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.PictureBox1, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents Panel2 As Panel
+    Friend WithEvents Panel1 As Panel
+    Friend WithEvents btnMaailmas As Button
+    Friend WithEvents BunifuElipse2 As Bunifu.Framework.UI.BunifuElipse
+    Friend WithEvents Button2 As Button
+    Friend WithEvents BunifuElipse4 As Bunifu.Framework.UI.BunifuElipse
+    Friend WithEvents Button1 As Button
+    Friend WithEvents BunifuElipse3 As Bunifu.Framework.UI.BunifuElipse
+    Friend WithEvents BunifuElipse5 As Bunifu.Framework.UI.BunifuElipse
+    Friend WithEvents BunifuElipse6 As Bunifu.Framework.UI.BunifuElipse
+    Friend WithEvents PictureBox1 As PictureBox
+    Friend WithEvents Panel3 As Panel
+    Friend WithEvents Panel4 As Panel
+    Friend WithEvents BunifuElipse1 As Bunifu.Framework.UI.BunifuElipse
+    Friend WithEvents BunifuElipse7 As Bunifu.Framework.UI.BunifuElipse
+    Friend WithEvents PictureBox2 As PictureBox
+    Friend WithEvents Label12 As Label
+    Friend WithEvents Label11 As Label
+    Friend WithEvents Label10 As Label
+    Friend WithEvents Label9 As Label
+    Friend WithEvents Label8 As Label
+    Friend WithEvents Label7 As Label
+    Friend WithEvents Label6 As Label
+    Friend WithEvents Label5 As Label
+    Friend WithEvents Label4 As Label
+    Friend WithEvents Label3 As Label
+    Friend WithEvents Label1 As Label
+    Friend WithEvents Label2 As Label
+    Friend WithEvents ComboBox1 As ComboBox
+    Friend WithEvents BunifuElipse8 As Bunifu.Framework.UI.BunifuElipse
+    Friend WithEvents Button3 As Button
+    Friend WithEvents BunifuElipse9 As Bunifu.Framework.UI.BunifuElipse
+    Friend WithEvents TextBox1 As TextBox
+End Class
